6a83227d0370717d5b7ab43f223275b972980d23
Blackwolf23 <33786887+Blackwolf23@users.noreply.github.com>
2018-04-14 15:52:35 +0200

    added tracers and basix expolsions
    The explosions and tracers are placeholders

--- src/FieldWarning/Assets/Units/Scripts/UnitBehaviour.cs
+++ src/FieldWarning/Assets/Units/Scripts/UnitBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
-public abstract class UnitBehaviour : SelectableBehavior,Matchable<Vector3> {
+public abstract class UnitBehaviour : SelectableBehavior,Matchable<Vector3>
+{
 	public UnitData data;
 
 	protected TerrainCollider Ground {
@@ -24,37 +25,45 @@ public abstract class UnitBehaviour : SelectableBehavior,Matchable<Vector3> {
 	public float timeLeft;
 	bool IsAlive;
 
+	public Transform turret;
+	public Transform barrel;
+	public Transform Shooting_root;
+	public ParticleSystem shooter_effect;
 
-    public AudioClip shotSound;
-    private AudioSource source;
-    private float shotVolume = 1.0F;
 
 
-    // Use this for initialization
-    public void Start() {		
-		destination = new Vector3(100, 0, -100);
+	public AudioClip shotSound;
+	private AudioSource source;
+	private float shotVolume = 1.0F;
+
+
+	// Use this for initialization
+	public void Start ()
+	{		
+		destination = new Vector3 (100, 0, -100);
 		transform.position = 100 * Vector3.down;
 		enabled = false;
 		height = 0;
-		data = UnitData.GenericUnit();
+		data = UnitData.GenericUnit ();
 		timeLeft = (float)data.weapon.ReloadTime;
 		health = data.maxHealth; //set the health to 10 (from UnitData.cs)
 		IsAlive = true;
-		setVisible(false);
+		setVisible (false);
 
-        // TODO refactor to explicitly tag enemy units when we add multiplayer
-        // TODO maybe refactor the constant out
-        this.tag = "Unit";
+		// TODO refactor to explicitly tag enemy units when we add multiplayer
+		// TODO maybe refactor the constant out
+		this.tag = "Unit";
 
-        source = GetComponent<AudioSource>();
-    }
+		source = GetComponent<AudioSource> ();
+	}
 
 	// Update is called once per frame
-	public void Update() {
+	public void Update ()
+	{
 
-		doMovement();
-		updateMapOrientation();
-		FireWeapon(FindClosestEnemy());
+		doMovement ();
+		updateMapOrientation ();
+		FireWeapon (FindClosestEnemy ());
 	}
 
 	/*void Countdown (int reloadtime) {
@@ -65,51 +74,75 @@ public abstract class UnitBehaviour : SelectableBehavior,Matchable<Vector3> {
 		return false;
 	}*/
 
-	public bool GetIsAlive() {
+	public bool GetIsAlive ()
+	{
 		return IsAlive;
 	}
 	//used in platoon behaviour to find destroyed unit
 
-	public void SetNewHeathOrDestroy(int receivedDamage) {
+	public void SetNewHeathOrDestroy (int receivedDamage)
+	{
 		health -= receivedDamage; //This one should be self explanatory
 		if (health < 1) {
 			IsAlive = false;
-            platoon.units.Remove(this);
+			platoon.units.Remove (this);
 		
-			Destroy(this.gameObject);
+			Destroy (this.gameObject);
 
-            // TODO destroy ghosts too
-            if (platoon.units.Count == 0) {
-                Destroy(platoon.gameObject);
-                // TODO remove from selection if part of one
-            }
+			// TODO destroy ghosts too
+			if (platoon.units.Count == 0) {
+				Destroy (platoon.gameObject);
+				// TODO remove from selection if part of one
+			}
 
 			return;
 		}
 	}
 
-	public bool FireWeapon(GameObject target) {
+
+
+	void FireBullet (GameObject target)
+	{
+		Vector3 direction_to_target = target.transform.position - turret.position;
+		Quaternion rotation_to_target = Quaternion.LookRotation (direction_to_target);
+
+		turret.eulerAngles = new Vector3 (0, rotation_to_target.eulerAngles.y, 0);
+		barrel.localEulerAngles = new Vector3 (rotation_to_target.eulerAngles.x, 0, 0);
+
+		Shooting_root.LookAt (target.transform);
+		shooter_effect.Play ();
+
+
+	}
+
+
+	public bool FireWeapon (GameObject target)
+	{
 		timeLeft -= Time.deltaTime;
 
 		if (timeLeft < 0) {
 		
 			Debug.Log ("weapon fired");
             
-            source.PlayOneShot(shotSound, shotVolume);
+			source.PlayOneShot (shotSound, shotVolume);
 
-            System.Random rnd = new System.Random();
-			int chance = rnd.Next(1, 100);
-            if (chance < data.weapon.Accuracy) {
+			FireBullet (target);
 
-				Debug.Log("shot fired and hit");
-				target.GetComponent<UnitBehaviour>()
-                        .SetNewHeathOrDestroy(data.weapon.Damage);
+
+
+			System.Random rnd = new System.Random ();
+			int chance = rnd.Next (1, 100);
+			if (chance < data.weapon.Accuracy) {
+
+				Debug.Log ("shot fired and hit");
+				target.GetComponent<UnitBehaviour> ()
+                        .SetNewHeathOrDestroy (data.weapon.Damage);
 				timeLeft = (float)data.weapon.ReloadTime;
 				return true;
 			}
 
 			timeLeft = (float)data.weapon.ReloadTime;
-			Debug.Log("shot fired and missed");
+			Debug.Log ("shot fired and missed");
 			return false;
 
 		}
@@ -117,29 +150,31 @@ public abstract class UnitBehaviour : SelectableBehavior,Matchable<Vector3> {
 		return false;
 	}
 
-	public GameObject FindClosestEnemy() {
-        GameObject[] units = GameObject.FindGameObjectsWithTag("Unit");
+	public GameObject FindClosestEnemy ()
+	{
+		GameObject[] units = GameObject.FindGameObjectsWithTag ("Unit");
 		GameObject Target = null;
-        Team thisTeam = this.platoon.team;
+		Team thisTeam = this.platoon.team;
         
 		for (int i = 0; i < (int)units.Length; i++) {
-            // Filter out friendlies:
-            if (units[i].GetComponent<UnitBehaviour>().platoon.team == thisTeam)
-                continue;
+			// Filter out friendlies:
+			if (units [i].GetComponent<UnitBehaviour> ().platoon.team == thisTeam)
+				continue;
 
-            // See if they are in range of weapon:
-            var distance = Vector3.Distance(units[i].transform.position, transform.position);
-            if (distance < data.weapon.FireRange) {
-                return units[i];
+			// See if they are in range of weapon:
+			var distance = Vector3.Distance (units [i].transform.position, transform.position);
+			if (distance < data.weapon.FireRange) {
+				return units [i];
 			}
-        }
-        Debug.Log("null");
-        return Target;
+		}
+		Debug.Log ("null");
+		return Target;
 	}
 
-	public abstract void updateMapOrientation();
+	public abstract void updateMapOrientation ();
 
-	public void setPlatoon(PlatoonBehaviour p) {
+	public void setPlatoon (PlatoonBehaviour p)
+	{
 		platoon = p;
 	}
 	/*public override void setDestination(Vector3 v)
@@ -155,63 +190,73 @@ public abstract class UnitBehaviour : SelectableBehavior,Matchable<Vector3> {
         platoon.getDestinationFromGhost();
     }*/
 
-	public float getHealth() {
+	public float getHealth ()
+	{
 		return health;
 	}
 
-	public void setHealth(float health)	{
+	public void setHealth (float health)
+	{
 		this.health = health;
 	}
 
-	public override PlatoonBehaviour getPlatoon() {
+	public override PlatoonBehaviour getPlatoon ()
+	{
 		return platoon;
 	}
 
-	public void setUnitDestination(Vector3 v) {
+	public void setUnitDestination (Vector3 v)
+	{
 
 		var diff = (v - transform.position).normalized;
 		setFinalOrientation (v, diff.getRadianAngle ());
 	}
 
-	public void setFinalOrientation(Vector3 d, float heading) {
+	public void setFinalOrientation (Vector3 d, float heading)
+	{
 		gotDestination = true;
 		destination = d;
 		setUnitFinalHeading (heading);
         
 	}
 
-	public void setUnitFinalFacing(Vector3 v) {
+	public void setUnitFinalFacing (Vector3 v)
+	{
 		var diff = (v - destination).normalized;
 		setUnitFinalHeading (diff.getRadianAngle ());
 	}
 
-	public virtual void setUnitFinalHeading(float heading)	{
+	public virtual void setUnitFinalHeading (float heading)
+	{
 		finalHeading = heading;
 	}
 
-	protected abstract void doMovement();
+	protected abstract void doMovement ();
 
-	public void setLayer(int l) {
+	public void setLayer (int l)
+	{
 		gameObject.layer = l;
 	}
 
-	protected abstract Renderer[] getRenderers();
+	protected abstract Renderer[] getRenderers ();
 
-	public void setVisible(bool vis) {
-		var renderers = getRenderers(); 
+	public void setVisible (bool vis)
+	{
+		var renderers = getRenderers (); 
 		foreach (var r in renderers) {
 			r.enabled = vis;
 		}
 
 		if (vis) {
-			setLayer(LayerMask.NameToLayer("Selectable"));
+			setLayer (LayerMask.NameToLayer ("Selectable"));
 
 		} else {
-			setLayer(LayerMask.NameToLayer("Ignore Raycast"));
+			setLayer (LayerMask.NameToLayer ("Ignore Raycast"));
 		}
 	}
-    
-	protected float unwrap(float f)	{
+
+	protected float unwrap (float f)
+	{
 		while (f > Mathf.PI)
 			f -= 2 * Mathf.PI;
 		while (f < -Mathf.PI)
@@ -219,26 +264,30 @@ public abstract class UnitBehaviour : SelectableBehavior,Matchable<Vector3> {
 		return f;
 	}
 
-	protected float getHeading() {
-		return (destination - transform.position).getDegreeAngle();
+	protected float getHeading ()
+	{
+		return (destination - transform.position).getDegreeAngle ();
 	}
 
 
-	public void setMatch(Vector3 match)	{
-		setUnitDestination(match);
+	public void setMatch (Vector3 match)
+	{
+		setUnitDestination (match);
 	}
 
-	public float getScore(Vector3 matchee) {
+	public float getScore (Vector3 matchee)
+	{
 		return (matchee - transform.position).magnitude;
 	}
 
-	public abstract void setOriginalOrientation(Vector3 pos, Quaternion rotation, bool wake = true);
+	public abstract void setOriginalOrientation (Vector3 pos, Quaternion rotation, bool wake = true);
 
 
-	protected void WakeUp()	{
+	protected void WakeUp ()
+	{
 		enabled = true;
 		setVisible (true);
 	}
 
-	public abstract bool ordersComplete();
+	public abstract bool ordersComplete ();
 }
---
+++ src/FieldWarning/Assets/bullets and tracers/BulletBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+
+
+	Vector3 start_position;
+	//staring position of the shell
+	Vector3 end_position;
+	//end positin
+	int Vellocity;
+	int arc;
+	//will be used later so shells arent lasers
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
