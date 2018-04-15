1ca811e9685a5a473e183184ac6287fd2bf3fbe1
pvutov <github@pvutov.imap.cc>
2018-04-15 04:55:21 +0200

    class name fix

--- src/FieldWarning/Assets/Prefabs/Camera/SlidingCameraBehaviour.cs
+++ src/FieldWarning/Assets/Prefabs/Camera/SlidingCameraBehaviour.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrbitCameraBehaviour : MonoBehaviour {
+public class SlidingCameraBehaviour : MonoBehaviour {
 
     [Header("Translational Movement")]
     [SerializeField] private float baseMovementspeed = 1.5f;
