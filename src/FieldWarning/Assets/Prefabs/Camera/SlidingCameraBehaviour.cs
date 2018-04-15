fbd060716233eba6056f89cf5429a8d89ca18452
pvutov <github@pvutov.imap.cc>
2018-04-15 05:03:05 +0200

    camera commit attempt 2

--- src/FieldWarning/Assets/Prefabs/Camera/SlidingCameraBehaviour.cs
+++ src/FieldWarning/Assets/Prefabs/Camera/SlidingCameraBehaviour.cs
@@ -1,126 +1,131 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SlidingCameraBehaviour : MonoBehaviour {
 
     [Header("Translational Movement")]
-    [SerializeField] private float baseMovementspeed = 1.5f;
+    [SerializeField]
+    private float panSpeed = 20f;
 
     [Header("Rotational Movement")]
-    [SerializeField] private float horizontalRotationSpeed = 5;
-    [SerializeField] private float verticalRotationSpeed = .1f;
-    [SerializeField] private float upperAngleLimit = 20;
-    [SerializeField] private float lowerAngleLimit = 80;
+    [SerializeField]
+    private float horizontalRotationSpeed = 400f;
+    [SerializeField]
+    private float verticalRotationSpeed = 400f;
+    [SerializeField]
+    private float maxCameraAngle = 85;
+    [SerializeField]
+    private float minCameraAngle = 5;
 
     [Header("Zoom Level")]
-    [SerializeField] private float zoomSpeed = 1f;
-    public int InitialZoomLevel = 0; //The Zoom Level at which the camera is set on level load.
-    [SerializeField] private int currentZoomLevel; //The current Zoom Level of the camera.
-    [SerializeField] private int minimumZoomLevel = 2;
-    [SerializeField] private float minZoomLevelDistance = 0.5f;
-    [SerializeField] private int maximumZoomLevel = 20;
+    [SerializeField]
+    private float zoomSpeed = 1000;
+    [SerializeField]
+    private float zoomTiltSpeed = 3;
+    [SerializeField]
+    private int minAltitude = 2;
+    [SerializeField]
+    private int maxAltitude = 2000;
 
-    private Transform orbitPoint; //The transform around which the camera orbits.
-    private Vector3 targetLocalPosition;
-    private bool lerpingToTarget;
-    private float distanceToStopLerpingToTarget = 0.01f;
     private Camera cam;
 
-    private void Awake()
-    {
-        orbitPoint = transform.parent;
+    private void Awake() {
         cam = GetComponent<Camera>();
     }
 
 
-    private void Start()
-    {
-        targetLocalPosition = new Vector3(0, 1, 0); //Initializes the target local position of the camera.
+    private void Start() {
 
-        ChangeZoomLevel(InitialZoomLevel); //Sets the camera's zoom level to its initial value.
-	}
-	
-	private void Update()
-    {
-        //If movement key is received, move the orbit point.
-        if (Input.GetKey(KeyCode.W))
-        {
-            MoveOrbitPoint(Vector3.forward);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            MoveOrbitPoint(Vector3.back);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            MoveOrbitPoint(Vector3.left);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            MoveOrbitPoint(Vector3.right);
-        }
-
-        //If mouse scroll wheel moved, change the zoom level.
-        if (Input.mouseScrollDelta.y < -0.5f)
-        {
-            ChangeZoomLevel(currentZoomLevel + 1);
-        }
-
-        if (Input.mouseScrollDelta.y > 0.5f)
-        {
-            ChangeZoomLevel(currentZoomLevel - 1);
-        }
-
-        RotateCamera(); //Run logic for rotating the camera.
-        
-        //Lerps the position of the actual local position toward the target local position.
-        cam.transform.localPosition = Vector3.Lerp(cam.transform.localPosition, targetLocalPosition, Time.deltaTime * zoomSpeed); 
-
-
-        transform.LookAt(orbitPoint); //Makes sure the camera is always looking at the Orbit Point.
-	}
-
-    private void MoveOrbitPoint(Vector3 movementVector)
-    {
-        var off = targetLocalPosition;
-        off.y = 0;
-        movementVector = Quaternion.FromToRotation(Vector3.forward, off) * -movementVector;
-        orbitPoint.position += Time.deltaTime * baseMovementspeed * movementVector * targetLocalPosition.magnitude;
-    }
-    
-    private void ChangeZoomLevel(int newZoomLevel)
-    {
-        //Makes sure the new zoom level is within its limits.
-        if (newZoomLevel < minimumZoomLevel)
-        {
-            newZoomLevel = minimumZoomLevel;
-        }
-
-        if (newZoomLevel > maximumZoomLevel)
-        {
-            newZoomLevel = maximumZoomLevel;
-        }
-
-        //The new target local position is the direction of the target from the orbit point multiplied by the zoom distance.
-        targetLocalPosition = ((cam.transform.localPosition - orbitPoint.position).normalized * minZoomLevelDistance) * newZoomLevel * newZoomLevel;
-
-        //Sets the current zoom level to the new zoom level.
-        currentZoomLevel = newZoomLevel;
     }
 
-    private void RotateCamera()
-    {
-        if (Input.GetMouseButton(2))
-        {
-            var dy = -Input.GetAxis("Mouse Y");
-            if ((Vector3.Angle(targetLocalPosition, Vector3.up) > upperAngleLimit || dy < 0) && (Vector3.Angle(targetLocalPosition, Vector3.up) < lowerAngleLimit || dy > 0))
-            {
-                targetLocalPosition = Vector3.RotateTowards(targetLocalPosition, Vector3.up, dy * verticalRotationSpeed, 0f);
+    private void Update() {
+        // Camera panning:
+        // TODO normalize before translation so panning does not slow down if we look down
+        var y = cam.transform.position.y;
+        cam.transform.Translate(Input.GetAxis("Horizontal") * Vector3.right * panSpeed * Time.deltaTime);
+        cam.transform.Translate(Input.GetAxis("Vertical") * Vector3.forward * panSpeed * Time.deltaTime);
+
+        // Panning shouldn't change cam altitude:
+        cam.transform.position = new Vector3(cam.transform.position.x, y, cam.transform.position.z);
+
+        // Zoom:
+        AimedZoom();
+
+        if (Input.GetMouseButton(2)) {
+            RotateCamera();
+        }
+    }
+
+    /*
+     * When zooming in we gradually approach whatever the cursor is pointing at
+     */
+    private void AimedZoom() {
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        // Zoom toward cursor and gradually tilt camera up:
+        if (scroll > 0) {
+            // Use a ray from the cursor to find what we'll be zooming into:
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            // If the cursor is not pointing at anything, zooming in is forbidden:
+            if (!Physics.Raycast(ray, out hit)) {
+                return;
             }
-            var dx = Input.GetAxis("Mouse X");
-            targetLocalPosition = Quaternion.AngleAxis(dx * horizontalRotationSpeed, Vector3.up) * targetLocalPosition;
+
+            cam.transform.position = Vector3.MoveTowards(cam.transform.position,
+                                                        hit.point,
+                                                        scroll * zoomSpeed * Time.deltaTime);
+        }
+
+        // Zoom out:
+        if (scroll < 0) {
+            cam.transform.Translate(Vector3.forward * scroll * zoomSpeed * Time.deltaTime);
+        }
+
+        // Tilt camera up/down slightly and limit its angle and altitude:
+        if (scroll != 0) {
+            cam.transform.Rotate(Vector3.left * zoomSpeed * Time.deltaTime * zoomTiltSpeed * scroll);
+
+            ClampCameraAngle(scroll > 0);
+            ClampCameraAltitude();
         }
     }
-    
+
+    private void RotateCamera() {
+        var dy = Input.GetAxis("Mouse Y") * verticalRotationSpeed * Time.deltaTime;
+        var dx = -Input.GetAxis("Mouse X") * horizontalRotationSpeed * Time.deltaTime;
+        cam.transform.eulerAngles -= new Vector3(dy, dx, 0);
+
+        ClampCameraAngle(dy > 0);
+    }
+
+    /*
+     * So we don't look too far up or down:
+     */
+    private void ClampCameraAngle(bool wasRotatedDownwards) {
+        //  TODO: TO fix gimbal lock, we need to clamp with quaternions; see https://forum.unity.com/threads/how-do-i-clamp-a-quaternion.370041/
+
+        //Debug.LogFormat("{0}; {1}; {2} \n", Quaternion.Angle(cam.transform.rotation, Quaternion.Euler(0, 0, 0)), 
+
+        //    Quaternion.Angle(cam.transform.rotation, Quaternion.Euler((maxCameraAngle + minCameraAngle) / 2, 180, 0)), 
+
+        //    Quaternion.Angle(cam.transform.rotation, Quaternion.Euler((maxCameraAngle + minCameraAngle) / 2, 0, 0))
+        //    - Quaternion.Angle(cam.transform.rotation, Quaternion.Euler((maxCameraAngle + minCameraAngle) / 2, 180, 0)));
+
+        //if (Quaternion.Angle(cam.transform.rotation, Quaternion.Euler((maxCameraAngle + minCameraAngle) / 2, 0,0)) 
+        //    - Quaternion.Angle(cam.transform.rotation, Quaternion.Euler((maxCameraAngle + minCameraAngle) / 2, 180, 180)) 
+        //    > (maxCameraAngle + minCameraAngle) / 2) {
+
+        if (cam.transform.rotation.x > maxCameraAngle || cam.transform.eulerAngles.x < minCameraAngle) {
+            cam.transform.eulerAngles = new Vector3(wasRotatedDownwards ? minCameraAngle : maxCameraAngle,
+                                                    cam.transform.eulerAngles.y,
+                                                    cam.transform.eulerAngles.z);
+        }
+    }
+
+    private void ClampCameraAltitude() {
+        cam.transform.position = new Vector3(cam.transform.position.x,
+                                             Mathf.Clamp(cam.transform.position.y, minAltitude, maxAltitude),
+                                             cam.transform.position.z);
+    }
 }
