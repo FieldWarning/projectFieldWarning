05d7cbdc72187ef21125555dae13aa264a5baa1c
Petar Vutov <git@pvutov.imap.cc>
2018-08-04 11:54:50 +0200

    code style bullets and tracers

--- src/FieldWarning/Assets/bullets and tracers/Bullet.cs
+++ src/FieldWarning/Assets/bullets and tracers/Bullet.cs
@@ -16,30 +16,30 @@ using UnityEngine;
 
 namespace AssemblyCSharp
 {
-	public class Bullet
-	{
-		private Vector3 _startPosition;
-		private Vector3 _endPosition;
-		int _vellocity;
+    public class Bullet
+    {
+        private Vector3 _startPosition;
+        private Vector3 _endPosition;
+        int _vellocity;
 
         //will be used later so shells arent lasers
         int _arc;
-		
-		public Bullet (Vector3 start_position, Vector3 end_position, int vellocity, int arc, Boolean isHit)
-		{
-			_startPosition = start_position; 
-			_endPosition = end_position;
-			_vellocity = vellocity;
-			_arc = arc;
 
-			//If the shell is a miss end position will be recalculated into a
-			//random yet close position to the enemy unit
+        public Bullet(Vector3 start_position, Vector3 end_position, int vellocity, int arc, bool isHit)
+        {
+            _startPosition = start_position;
+            _endPosition = end_position;
+            _vellocity = vellocity;
+            _arc = arc;
 
-			if (!isHit) {
-				end_position.x = UnityEngine.Random.Range (-20.0f, 20.0f);
-				end_position.z = UnityEngine.Random.Range (-20.0f, 20.0f);
-			}
-		}	
-	}
+            //If the shell is a miss end position will be recalculated into a
+            //random yet close position to the enemy unit
+
+            if (!isHit) {
+                end_position.x = UnityEngine.Random.Range(-20.0f, 20.0f);
+                end_position.z = UnityEngine.Random.Range(-20.0f, 20.0f);
+            }
+        }
+    }
 }
 
