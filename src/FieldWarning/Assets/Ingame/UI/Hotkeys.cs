5511aaaf04268035ac3d0e264c30638e67cea7a9
Petar Vutov <git@pvutov.imap.cc>
2018-08-04 19:30:13 +0200

    refactor, code style

---
+++ src/FieldWarning/Assets/Ingame/UI/Hotkeys.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Ingame.UI
+{
+    public class Hotkeys
+    {
+        public static KeyCode Unload = KeyCode.U;
+        public static KeyCode Load = KeyCode.L;
+        public static KeyCode FirePos = KeyCode.T;
+    }
+}
