e83398644a025779c2185768218ba05d7b9ccc2b
Petar Vutov <git@pvutov.imap.cc>
2018-08-04 11:59:53 +0200

    code style camera

--- src/FieldWarning/Assets/Camera/CameraCollision.cs
+++ src/FieldWarning/Assets/Camera/CameraCollision.cs
@@ -1,16 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraCollision : MonoBehaviour {
+public class CameraCollision : MonoBehaviour
+{
+    // Use this for initialization
+    void Start() {}
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update() {}
 }
