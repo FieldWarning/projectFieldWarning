b2e844192571a64701cf5596f390087c1db0d403
gatchrat <henkolt@t-online.de>
2019-03-12 21:58:45 +0100

    Added a Capture Zone prefab and example
    Also code formating and folder structure fixes

--- src/FieldWarning/Assets/Ingame/CaptureZone/CaptureZone.cs
+++ src/FieldWarning/Assets/Ingame/CaptureZone/CaptureZone.cs
@@ -1,4 +1,17 @@
-﻿using PFW.Model.Game;
+﻿/**
+ * Copyright (c) 2017-present, PFW Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+using PFW.Model.Game;
 
 using System.Collections;
 using System.Collections.Generic;
@@ -7,13 +20,13 @@ namespace PFW.Ingame.UI
 {
     public class CaptureZone : MonoBehaviour
     {
-        public Material red;
-        public Material blue;
-        public Material neutral;
+        public Material Red;
+        public Material Blue;
+        public Material Neutral;
         //How many Points per tick this Zone gives
         public int Worth = 3;
         //Maybe take out all that owner stuff and simply use an int or otherwise shorten the code
-        private PlayerData owner;
+        private PlayerData _owner;
         //Vehicles Currently in the Zone (Maybe exclude all non-commander vehicles),maybe replace list with Array
         private List<VehicleBehaviour> _vehicles = new List<VehicleBehaviour>();
         // Start is called before the first frame update
@@ -28,41 +41,28 @@ namespace PFW.Ingame.UI
             bool redIncluded = false;
             bool blueIncluded = false;
             PlayerData newOwner = null;
-            for (int i = 0; i<_vehicles.Count;i++)
-            {
+            for (int i = 0; i < _vehicles.Count; i++) {
                 VehicleBehaviour vehicle = _vehicles.ToArray()[i];
-                if (vehicle.OrdersComplete())
-                {
+                if (vehicle.OrdersComplete()) {
                     newOwner = vehicle.Platoon.Owner;
                     //Names are USSR and NATO
-                    if (newOwner.Team.Name=="USSR")
-                    {
+                    if (newOwner.Team.Name == "USSR") {
                         redIncluded = true;
-                    }
-                    else
-                    {
+                    } else {
                         blueIncluded = true;
                     }
                 }
             }
-            if (redIncluded && blueIncluded ||(!redIncluded && !blueIncluded))
-            {
-                if (owner != null)
-                {
+            if (redIncluded && blueIncluded || (!redIncluded && !blueIncluded)) {
+                if (_owner != null) {
                     changeOwner(null);
                 }
-            }
-            else if (redIncluded)
-            {
-                if (owner != newOwner)
-                {
+            } else if (redIncluded) {
+                if (_owner != newOwner) {
                     changeOwner(newOwner);
                 }
-            }
-            else
-            {
-                if (owner != newOwner)
-                {
+            } else {
+                if (_owner != newOwner) {
                     changeOwner(newOwner);
                 }
             }
@@ -70,44 +70,34 @@ namespace PFW.Ingame.UI
         //needs to play sound
         private void changeOwner(PlayerData newOwner)
         {
-            if (owner != null)
-            {
-                owner.IncomeTick -= Worth;
+            if (_owner != null) {
+                _owner.IncomeTick -= Worth;
             }
-            if (newOwner != null)
-            {
+            if (newOwner != null) {
                 newOwner.IncomeTick += Worth;
             }
-            owner = newOwner;
-            if (owner != null)
-            {
-                if (owner.Team.Name == "USSR")
-                {
-                    this.GetComponent<MeshRenderer>().material = red;
+            _owner = newOwner;
+            if (_owner != null) {
+                if (_owner.Team.Name == "USSR") {
+                    this.GetComponent<MeshRenderer>().material = Red;
+                } else {
+                    this.GetComponent<MeshRenderer>().material = Blue;
                 }
-                else
-                {
-                    this.GetComponent<MeshRenderer>().material = blue;
-                }
+            } else {
+                this.GetComponent<MeshRenderer>().material = Neutral;
             }
-            else
-            {
-                this.GetComponent<MeshRenderer>().material = neutral;
-            }
-           
-            
+
+
         }
         private void OnTriggerEnter(Collider other)
         {
-            if (other.transform.parent != null &&other.transform.parent.GetComponent<VehicleBehaviour>() != null && other.transform.parent.GetComponent<VehicleBehaviour>().isActiveAndEnabled)
-            {
+            if (other.transform.parent != null && other.transform.parent.GetComponent<VehicleBehaviour>() != null && other.transform.parent.GetComponent<VehicleBehaviour>().isActiveAndEnabled) {
                 _vehicles.Add(other.transform.parent.GetComponent<VehicleBehaviour>());
             }
         }
         private void OnTriggerExit(Collider other)
         {
-            if (other.transform.parent.GetComponent<VehicleBehaviour>() != null)
-            {
+            if (other.transform.parent.GetComponent<VehicleBehaviour>() != null) {
                 _vehicles.Remove(other.transform.parent.GetComponent<VehicleBehaviour>());
             }
         }
