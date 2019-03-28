b3363a711f438f3b60a63f04710893e432987162
Max M <23467510+maximalum@users.noreply.github.com>
2019-03-28 02:31:12 -0500

    Fixed SpawnPointBehaviour.cs Refactoring
    Renamed `spawnQueue` to `_spawnQueue` and added missing `this` statements. Thanks fade.

--- src/FieldWarning/Assets/Ingame/UI/SpawnPointBehaviour.cs
+++ src/FieldWarning/Assets/Ingame/UI/SpawnPointBehaviour.cs
@@ -18,39 +18,37 @@ using UnityEngine;
 public class SpawnPointBehaviour : MonoBehaviour {
     public const float MIN_SPAWN_INTERVAL = 2f;
     public const float QUEUE_DELAY = 1f;
-    /* I broke C# law by using  */
     public Team @Team { get; private set; }
-    private Queue<GhostPlatoonBehaviour> spawnQueue { get; } = new Queue<GhostPlatoonBehaviour>();
-
+    
+    private Queue<GhostPlatoonBehaviour> _spawnQueue { get; } = new Queue<GhostPlatoonBehaviour>();
     private float _spawnTime = MIN_SPAWN_INTERVAL;
-
+    
     public void Awake() {
         this.Team = this.GetComponentInParent<Team>();
     }
 
     public void Start() {
         this.GetComponentInChildren<Renderer>().material.color = this.Team.Color;
-
         this.Team.Session.AddSpawnPoint(this);
     }
 
     public void Update() {
         // If there is no one in the spawn queue then don't continue.
-        if (!this.spawnQueue.Any()) return;
+        if (!this._spawnQueue.Any()) return;
 
         // Check if spawn time minus the current deltaTime is > 0 then exit method;
         if (this._spawnTime -= Time.deltaTime > 0) return;
 
         // NullReferenceException City - position is not set properly.
-        this.spawnQueue.Dequeue().Spawn(transform.position);
+        this._spawnQueue.Dequeue().Spawn(this.transform.position);
 
         // Increase spawn queue time if total amount in queue is greater than zero
         //  otherwise set to QUEUE_DELAY (1f) PS: Turnary > if statements
-        this._spawnTime = (this.spawnQueue.Count > 0) ?
+        this._spawnTime = (this._spawnQueue.Count > 0) ?
             this._spawnTime += MIN_SPAWN_INTERVAL : QUEUE_DELAY;
     }
 
     public void BuyPlatoons(List<GhostPlatoonBehaviour> ghostPlatoons) {
-        ghostPlatoons.ForEach (x => spawnQueue.Enqueue(x));
+        ghostPlatoons.ForEach (x => this._spawnQueue.Enqueue(x));
     }
 }
