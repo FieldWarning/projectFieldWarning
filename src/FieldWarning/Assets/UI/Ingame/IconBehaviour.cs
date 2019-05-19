FieldWarning/projectFieldWarning

commit 52a0917633bfb34bb0b6be68337c181d5ffe693b

Make SelectableBehavior standalone.

src/FieldWarning/Assets/UI/Ingame/IconBehaviour.cs
@@ -17,7 +17,7 @@ using PFW.Units;
 
 namespace PFW.UI.Ingame
 {
-    public class IconBehaviour : SelectableBehavior
+    public class IconBehaviour : MonoBehaviour
     {
         private static Color DEFAULT_COLOR = Color.cyan;
 
@@ -51,6 +51,11 @@ namespace PFW.UI.Ingame
 
             SetSelected(false);
             SetVisible(_visible);
+
+            transform.gameObject.AddComponent<SelectableBehavior>().Platoon =
+                    transform
+                            .parent
+                            .GetComponent<PlatoonBehaviour>();
         }
 
         public void SetLayer(int l)

src/FieldWarning/Assets/Scripts.meta
@@ -1,8 +1,0 @@
-fileFormatVersion: 2
-guid: 423c0d684069c90428161880f8457843
-folderAsset: yes
-DefaultImporter:
-  externalObjects: {}
-  userData: 
-  assetBundleName: 
-  assetBundleVariant: 

src/FieldWarning/Assets/Scripts/UI.meta
@@ -1,8 +1,0 @@
-fileFormatVersion: 2
-guid: 9c0bf8fb340ae3143a0df9da6d2915eb
-folderAsset: yes
-DefaultImporter:
-  externalObjects: {}
-  userData: 
-  assetBundleName: 
-  assetBundleVariant: 

src/FieldWarning/Assets/Scripts/UI/Ingame.meta
@@ -1,8 +1,0 @@
-fileFormatVersion: 2
-guid: d772853128ce65b40868470d34c5ddff
-folderAsset: yes
-DefaultImporter:
-  externalObjects: {}
-  userData: 
-  assetBundleName: 
-  assetBundleVariant: 

src/FieldWarning/Assets/Scripts/Unit.meta
@@ -1,8 +1,0 @@
-fileFormatVersion: 2
-guid: c8c311d385fd1c94e840ea6b38b2b78f
-folderAsset: yes
-DefaultImporter:
-  externalObjects: {}
-  userData: 
-  assetBundleName: 
-  assetBundleVariant: 

src/FieldWarning/Assets/UI/Ingame/BillboardBehavior.cs
@@ -18,7 +18,7 @@ namespace PFW.UI.Ingame
     /*
      * A billboard is a 2d texture that is always facing the camera.
      */
-    public class BillboardBehavior : SelectableBehavior
+    public class BillboardBehavior : MonoBehaviour
     {
         [SerializeField]
         private float ALTITUDE = 10f * TerrainConstants.MAP_SCALE;

src/FieldWarning/Assets/UI/Ingame/CompoundHealthbarBehaviour.cs
@@ -17,7 +17,7 @@ using PFW.Units;
 
 namespace PFW.UI.Ingame
 {
-    public class CompoundHealthbarBehaviour : SelectableBehavior
+    public class CompoundHealthbarBehaviour : MonoBehaviour
     {
         List<GameObject> objects;
 

commit e1b6979c47e5a793fe7ddde4019292d1fb64863b

Fix exception when clicking a ghost icon, introduced by 52a09176.

src/FieldWarning/Assets/UI/Ingame/IconBehaviour.cs
@@ -51,11 +51,6 @@ namespace PFW.UI.Ingame
 
             SetSelected(false);
             SetVisible(_visible);
-
-            transform.gameObject.AddComponent<SelectableBehavior>().Platoon =
-                    transform
-                            .parent
-                            .GetComponent<PlatoonBehaviour>();
         }
 
         public void SetLayer(int l)
@@ -66,9 +61,19 @@ namespace PFW.UI.Ingame
             gameObject.layer = l;
         }
 
-        public void SetSource(List<UnitDispatcher> list)
+        /// <summary>
+        /// Mark as associated to a set of non-ghost units.
+        /// </summary>
+        /// <param name="list"></param>
+        public void AssociateToRealUnits(List<UnitDispatcher> list)
         {
             _billboard.GetComponentInChildren<CompoundHealthbarBehaviour>().SetSource(list);
+
+            // Make selectable:
+            transform.gameObject.AddComponent<SelectableBehavior>().Platoon =
+                    transform
+                            .parent
+                            .GetComponent<PlatoonBehaviour>();
         }
 
         public void SetVisible(bool vis)
@@ -102,6 +107,7 @@ namespace PFW.UI.Ingame
             _billboard.GetComponent<Renderer>().material.color = color;
             _symbol.GetComponent<Renderer>().material.color = color;// (color + Color.white) / 2;
         }
+
 
         public void SetGhost()
         {

src/FieldWarning/Assets/Units/PlatoonBehaviour.cs
@@ -110,7 +110,7 @@ public partial class PlatoonBehaviour : MonoBehaviour
 
         Movement.SetDestination(Vector3.forward);
 
-        Icon.SetSource(Units);
+        Icon.AssociateToRealUnits(Units);
 
         IsInitialized = true;
     }
@@ -161,7 +161,7 @@ public partial class PlatoonBehaviour : MonoBehaviour
         Movement.SetDestination(destination.Destination);
         Movement.EndQueueing();
 
-        Icon.SetSource(Units);
+        Icon.AssociateToRealUnits(Units);
 
         GhostPlatoon.SetVisible(false);
 