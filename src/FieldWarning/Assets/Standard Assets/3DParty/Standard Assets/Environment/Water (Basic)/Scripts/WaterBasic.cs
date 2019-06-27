c5c6227c1e7b64a13e5e9e1bfee304fa14056eb7
Petar Vutov <github@pvutov.imap.cc>
2019-06-28 00:35:14 +0200

    Resolve some build issues with the new water shader.

--- src/FieldWarning/Assets/Standard Assets/3DParty/Standard Assets/Environment/Water (Basic)/Scripts/WaterBasic.cs
+++ src/FieldWarning/Assets/Standard Assets/3DParty/Standard Assets/Environment/Water (Basic)/Scripts/WaterBasic.cs
@@ -24,7 +24,8 @@ namespace UnityStandardAssets.Water
             float t = Time.time / 20.0f;
 
             Vector4 offset4 = waveSpeed * (t * waveScale);
-            Vector4 offsetClamped = new Vector4(Mathf.Repeat(offset4.x, 1.0f), Mathf.Repeat(offset4.y, 1.0f),
+            Vector4 offsetClamped = new Vector4(
+                Mathf.Repeat(offset4.x, 1.0f), Mathf.Repeat(offset4.y, 1.0f),
                 Mathf.Repeat(offset4.z, 1.0f), Mathf.Repeat(offset4.w, 1.0f));
             mat.SetVector("_WaveOffset", offsetClamped);
         }
