b17a3ca455e38857978a5207d473590740842f82
dannymcgee <dannymcgee@gmail.com>
2019-07-29 11:18:12 -0400

    Script components for controlling UI graphics

---
+++ src/FieldWarning/Assets/UI/Ingame/ButtonGFXController.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * Copyright (c) 2017-present, PFW Contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PFW.UI.Ingame
+{
+    [ExecuteAlways]
+    public class ButtonGFXController : UIGFXController
+    {
+        [Header("Components")]
+    #pragma warning disable 0649
+        [SerializeField]
+        private Image _colorSprite;
+
+        [SerializeField]
+        private Image _borderSprite;
+
+        [SerializeField]
+        private TextMesh _textMesh;
+    #pragma warning restore 0649
+
+        private float _colorAlpha = 0.7f;
+        private float _borderAlpha = 0.525f;
+
+        protected override void Start()
+        {
+            base.Start();
+            _colorSprite.color = new Color(_baseColor.r, _baseColor.g, _baseColor.b, _colorAlpha);
+            _borderSprite.color = new Color(_accentColor.r, _accentColor.g, _accentColor.b, _borderAlpha);
+            _textMesh.color = Color.white;
+        }
+    }
+}
