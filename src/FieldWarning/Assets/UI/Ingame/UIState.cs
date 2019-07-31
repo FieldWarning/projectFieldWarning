2c1024eaad340cdb6a64dc8f1810429222496ea8
dannymcgee <dannymcgee@gmail.com>
2019-07-31 03:37:57 -0400

    Tweak Merge logic and add comments

--- src/FieldWarning/Assets/UI/Ingame/UIState.cs
+++ src/FieldWarning/Assets/UI/Ingame/UIState.cs
@@ -89,12 +89,16 @@ namespace PFW.UI.Ingame
 
         public static UIState Merge(params List<ColorState>[] states)
         {
+            // FIXME: Can someone who's heard of algorithms before take a look at this?
+            // I suspect this is not exactky optimal
+
             var merged = new List<ColorState>();
 
             for (int i = 0; i < states.Length - 1; i++) {
-                var aColors = states[i];
+                var aColors = (i == 0) ? states[i] : merged;
                 var bColors = states[i + 1];
 
+                // ColorStates which are unique to aColors
                 List<ColorState> original = aColors.Where(aColor => {
                     foreach (ColorState bColor in bColors)
                         if (bColor.Component == aColor.Component)
@@ -102,6 +106,7 @@ namespace PFW.UI.Ingame
                     return true;
                 }).ToList();
 
+                // ColorStates which are unique to bColors
                 List<ColorState> additions = bColors.Where(bColor => {
                     foreach (ColorState aColor in aColors)
                         if (aColor.Component == bColor.Component)
@@ -109,9 +114,12 @@ namespace PFW.UI.Ingame
                     return true;
                 }).ToList();
 
+                // ColorStates in bColors which modify states in aColors
                 List<ColorState> modifications = bColors.Where(bColor =>
                         (!additions.Contains(bColor))).ToList();
 
+                // Merge the changed values in bColors modifications
+                // with the unchanged values from the corresponding aColors
                 foreach (ColorState mColor in modifications) {
                     ColorState aColor = aColors.Find(a => a.Component == mColor.Component);
                     mColor.Color = (mColor.Color != null) ? mColor.Color : aColor.Color;
