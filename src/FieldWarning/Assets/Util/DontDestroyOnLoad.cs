c028d2a43563f4f5129744e7437b96eebd6fc518
triangle345 <efeldman21@gmail.com>
2020-01-05 15:22:49 -0500

    added more missing files

---
+++ src/FieldWarning/Assets/Util/DontDestroyOnLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+
+    public int id = 0;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+}
