bc83d2f30c27d7ac24e825cf2e365fb5fa21c997
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2020-04-19 04:50:03 +0200

    small update

--- pfw-server/PFW_OfficialHub/Controllers/Db.cs
+++ pfw-server/PFW_OfficialHub/Controllers/Db.cs
@@ -8,36 +8,36 @@ using Shared;
 
 namespace PFW_OfficialHub.Controllers
 {
+    /// <summary>
+    /// !!!Do not include connection string in repository!!!
+    /// </summary>
     public static class Db
     {
-        /// <summary>
-        /// !!!Do not include connection string in repository!!!
-        /// </summary>
         static Db()
         {
-            string conStr;
-            try
-            {
-                conStr = File.ReadAllText("C:\\Users\\admin\\Documents\\pfwdb.txt");
+            var path = "C:\\Users\\admin\\Documents\\pfwdb.txt";
+            //TODO save default path for dev purposes
 
-            }
-            catch (FileNotFoundException e)
+            while (!File.Exists(path))
             {
                 Console.WriteLine("Db connection string file not found, add path to file (will crash if no file): ");
-                var path = Console.ReadLine();
-                conStr = File.ReadAllText();
+                path = Console.ReadLine();
             }
-
-            Client = new MongoClient(conStr);
+            Console.WriteLine("Found db file in => "+path);
+            string conStr = File.ReadAllText(path);
 
             Task.Run(delegate {
                 while (true) {
-                    Task.Delay(10000);
+                    Task.Delay(10000).Wait();
                     OnlinePlayers.DeleteManyAsync(x => x.LastOnline < DateTime.UtcNow - TimeSpan.FromSeconds(15));
                 }
             });
         }
 
+
+        /// <summary>
+        /// To get access to online db, ask a dev
+        /// </summary>
         public static IMongoClient Client = new MongoClient();
         public static IMongoDatabase Database = Client.GetDatabase("FieldWarningServer");
 
@@ -45,5 +45,6 @@ namespace PFW_OfficialHub.Controllers
         
         public static IMongoCollection<User> Users = Database.GetCollection<User>("Users");
         public static IMongoCollection<Player> OnlinePlayers = Database.GetCollection<Player>("Players");
+        public static void Init() => Console.WriteLine("Db Init");
     }
 }
