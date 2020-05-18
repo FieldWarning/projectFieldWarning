87777955c0c922f15f0d7f08fe971b3b72be442f
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2020-05-18 10:37:30 +0200

    added join server functionality

--- pfw-server/Shared/CustomTypes.cs
+++ pfw-server/Shared/CustomTypes.cs
@@ -6,37 +6,5 @@ using System.Threading.Tasks;
 
 namespace Shared
 {
-    /// <summary>
-    /// Add is quick, delete is slow, lock is not guaranteed
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class LockList<T>
-    {
-        private List<T> Items = new List<T>();
-        object CurrentLock = new object();
 
-        public List<T> Get(Predicate<T> exp) => Items.FindAll(exp);
-        public T Single(Predicate<T> exp) => Items.Find(exp);
-
-        public void Add(IEnumerable<T> items)
-        {
-            lock (CurrentLock)
-                Items.AddRange(items);
-        }
-        public void Add(T item)
-        {
-            lock (CurrentLock)
-            {
-                Items.Add(item);
-            }
-        }
-
-        public void Delete(Predicate<T> exp)
-        {
-            lock (CurrentLock)
-            {
-                Items.RemoveAll(exp);
-            }
-        }
-    }
 }
