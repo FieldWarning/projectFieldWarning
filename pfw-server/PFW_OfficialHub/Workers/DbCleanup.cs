ed1c0deceec62e1eaa3fbc879323fc91142de0a8
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2020-05-18 11:14:32 +0200

    added server heartbeat functionality

--- pfw-server/PFW_OfficialHub/Workers/DbCleanup.cs
+++ pfw-server/PFW_OfficialHub/Workers/DbCleanup.cs
@@ -1,11 +1,26 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Database;
+using MongoDB.Driver;
 namespace PFW_OfficialHub.Workers
 {
-    public class DbCleanup
-    {
+    public class DbCleanup {
+        private Task playerCleanupTask;
+        private Task lobbyCleanupTask;
+        public void StartPlayerCleanup() {
+            playerCleanupTask = Task.Run(async delegate {
+                while (true) {
+                    await Task.Delay(1000 * 10);
+                    Db.Players.DeleteMany(x => x.LastSeen < DateTime.UtcNow - TimeSpan.FromSeconds(15));
+                }
+            });
+        }
+
+        public void StopPlayerCleanup() {
+            playerCleanupTask.Dispose();
+        }
+
     }
 }
