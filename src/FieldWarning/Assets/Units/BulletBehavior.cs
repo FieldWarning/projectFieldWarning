355191091d518199b5e2abb97bc0e19afb340ec9
pvutov <github@pvutov.imap.cc>
2020-05-31 15:33:45 +0200

    Namespace the old BulletBehavior class.

--- src/FieldWarning/Assets/Units/BulletBehavior.cs
+++ src/FieldWarning/Assets/Units/BulletBehavior.cs
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Copyright (c) 2017-present, PFW Contributors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
@@ -14,133 +14,137 @@
 using AssemblyCSharp;
 using UnityEngine;
 
-public class BulletBehavior : MonoBehaviour
+namespace PFW.Units
 {
-    public Bullet bullet; //contains attributes for the shell
-    [Header("Explosion you want to appear when shell hits the target or ground")]
-    public GameObject ExplosionPrefab;
-    [Header("Trail emitter of this shell prefab - to be disabled on hit")]
-    public ParticleSystem TrailEmitter;
-
-    private Rigidbody rigid;
-
-    private float LaunchAngle;
-    private Transform StartingTransform;
-    private Vector3 TargetCoordinates;
-    //class used in the weapon behaviour to set stats of the shell
-    public void SetUp(Transform position, Vector3 target, float launchAngel)
+    public class BulletBehavior : MonoBehaviour
     {
-        StartingTransform = position;
-        TargetCoordinates = target;
-        LaunchAngle = launchAngel;
-    }
+        public Bullet Bullet; // contains attributes for the shell
+        [Header("Explosion you want to appear when shell hits the target or ground")]
+        public GameObject ExplosionPrefab;
+        [Header("Trail emitter of this shell prefab - to be disabled on hit")]
+        public ParticleSystem TrailEmitter;
 
-    void Start()
-    {
-        bullet = new Bullet();
-        rigid = GetComponent<Rigidbody>();
+        private Rigidbody _rigid;
 
-        rigid.useGravity = false;
-        rigid.isKinematic = true; // means that rigidbody is moved by script and does not affected by physics engine
+        private float _launchAngle;
+        private Transform _startingTransform;
+        private Vector3 _targetCoordinates;
 
-        Launch();
-    }
-
-    private float Gravity = 9.8F;
-    private float ForwardSpeed = 0F;
-    private float VerticalSpeed = 0F;
-    public void Launch()
-    {
-        Vector3 projectileXZPos = new Vector3(transform.position.x, 0.0f, transform.position.z);
-        Vector3 targetXZPos = new Vector3(TargetCoordinates.x, 0.0f, TargetCoordinates.z);
-
-        // rotate the object to face the target
-        transform.LookAt(targetXZPos);
-
-        // formula
-        float R = Vector3.Distance(projectileXZPos, targetXZPos);
-        //float G = 9.8F;     //TODO Readjust for our scale usinga utility class
-        float tanAlpha = Mathf.Tan(LaunchAngle * Mathf.Deg2Rad);
-        float H = TargetCoordinates.y - transform.position.y;
-
-        // calculate the local space components of the velocity
-        // required to land the projectile on the target object
-        //float Vz = Mathf.Sqrt(G * R * R / (2.0f * (H - R * tanAlpha)));
-
-        float Vz = 20F;
-
-        float DistanceToHighestPoint = R / 2;
-        float TimeToHighestPoint = DistanceToHighestPoint / Vz;
-        float GravityEffectToHighestPoint = Gravity * TimeToHighestPoint;
-
-        float Vy = GravityEffectToHighestPoint;
-
-        ForwardSpeed = Vz;
-        VerticalSpeed = Vy;
-
-        //Debug.LogFormat("BulletBehavior.Launch: ForwardSpeed={0}, VerticalSpeed={1}, LaunchAngle={2}, R={3}, tanALpha={4}, H={5}",
-        //    ForwardSpeed, VerticalSpeed, LaunchAngle, R, tanAlpha, H);
-
-        // create the velocity vector in local space and get it in global space
-
-        //BulletBehavior.Launch: ForwardSpeed=NaN, VerticalSpeed=NaN, LaunchAngle=60, R=15.60759, tanALpha=1.732051, H=-0.8795097
-    }
-
-    bool dead = false;
-    float prevDistanceToTarget = 100000F;
-    void Update()
-    {
-        if(dead) {
-            return;
+        //class used in the weapon behaviour to set stats of the shell
+        public void SetUp(Transform position, Vector3 target, float launchAngel)
+        {
+            _startingTransform = position;
+            _targetCoordinates = target;
+            _launchAngle = launchAngel;
         }
 
-        transform.Translate(ForwardSpeed * Vector3.forward * Time.deltaTime + VerticalSpeed * Vector3.up * Time.deltaTime);
+        private void Start()
+        {
+            Bullet = new Bullet();
+            _rigid = GetComponent<Rigidbody>();
 
-        VerticalSpeed = VerticalSpeed - (Gravity * Time.deltaTime);
+            _rigid.useGravity = false;
+            _rigid.isKinematic = true; // means that rigidbody is moved by script and does not affected by physics engine
+
+            Launch();
+        }
+
+        private float Gravity = 9.8F * PFW.Constants.MAP_SCALE;
+        private float ForwardSpeed = 0F;
+        private float VerticalSpeed = 0F;
+
+        public void Launch()
+        {
+            Vector3 projectileXZPos = new Vector3(transform.position.x, 0.0f, transform.position.z);
+            Vector3 targetXZPos = new Vector3(_targetCoordinates.x, 0.0f, _targetCoordinates.z);
+
+            // rotate the object to face the target
+            transform.LookAt(targetXZPos);
+
+            // formula
+            float R = Vector3.Distance(projectileXZPos, targetXZPos);
+            //float G = 9.8F;     //TODO Readjust for our scale usinga utility class
+            float tanAlpha = Mathf.Tan(_launchAngle * Mathf.Deg2Rad);
+            float H = _targetCoordinates.y - transform.position.y;
+
+            // calculate the local space components of the velocity
+            // required to land the projectile on the target object
+            //float Vz = Mathf.Sqrt(G * R * R / (2.0f * (H - R * tanAlpha)));
+
+            float Vz = 20F;
+
+            float DistanceToHighestPoint = R / 2;
+            float TimeToHighestPoint = DistanceToHighestPoint / Vz;
+            float GravityEffectToHighestPoint = Gravity * TimeToHighestPoint;
+
+            float Vy = GravityEffectToHighestPoint;
+
+            ForwardSpeed = Vz;
+            VerticalSpeed = Vy;
+
+            //Debug.LogFormat("BulletBehavior.Launch: ForwardSpeed={0}, VerticalSpeed={1}, LaunchAngle={2}, R={3}, tanALpha={4}, H={5}",
+            //    ForwardSpeed, VerticalSpeed, LaunchAngle, R, tanAlpha, H);
+
+            // create the velocity vector in local space and get it in global space
+
+            //BulletBehavior.Launch: ForwardSpeed=NaN, VerticalSpeed=NaN, LaunchAngle=60, R=15.60759, tanALpha=1.732051, H=-0.8795097
+        }
+
+        bool dead = false;
+        float prevDistanceToTarget = 100000F;
+        private void Update()
+        {
+            if (dead)
+            {
+                return;
+            }
+
+            transform.Translate(ForwardSpeed * Vector3.forward * Time.deltaTime + VerticalSpeed * Vector3.up * Time.deltaTime);
+
+            VerticalSpeed = VerticalSpeed - (Gravity * Time.deltaTime);
 
 
-        // small trick to detect if shell is reached the target
-        float distanceToTarget = Vector3.Distance(transform.position, TargetCoordinates);
-        if(distanceToTarget > prevDistanceToTarget) {
+            // small trick to detect if shell is reached the target
+            float distanceToTarget = Vector3.Distance(transform.position, _targetCoordinates);
+            if (distanceToTarget > prevDistanceToTarget)
+            {
+                Explode();
+            }
+            prevDistanceToTarget = distanceToTarget;
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
             Explode();
         }
-        prevDistanceToTarget = distanceToTarget;
-    }
 
-    void OnTriggerEnter(Collider other)
-    {
-        Explode();
-    }
+        void Explode()
+        {
+            dead = true;
+            if (ExplosionPrefab != null)
+            {
+                // instantiate explosion
+                GameObject explosion = Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
+                // destroy it in 10 seconds to not trash the scene
+                Destroy(explosion, 10F);
+            }
 
-    void Explode() {
-        dead = true;
-        if(ExplosionPrefab != null) {
-            // instantiate explosion
-            GameObject explosion = Instantiate(ExplosionPrefab, transform.position, Quaternion.identity);
-            // destroy it in 10 seconds to not trash the scene
-            Destroy(explosion, 10F);
+            if (TrailEmitter != null)
+            {
+                var emission = TrailEmitter.emission;
+                emission.enabled = false;
+            }
+
+            Destroy(gameObject, 10F);
         }
 
-        if(TrailEmitter != null) {
-            var emission = TrailEmitter.emission;
-            emission.enabled = false;
-        }
+        //public void setBullet(Vector3 StartPosition, Vector3 EndPosition, float Vellocity = 30, int arc = 60)
+        //{
+        //    bullet._startPosition = StartPosition;
+        //    bullet._endPosition = EndPosition;
+        //    bullet._vellocity = Vellocity;
+        //    bullet._arc = 60;
+        //}
 
-        Destroy(gameObject, 10F);
     }
-
-
-
-
-
-    //public void setBullet(Vector3 StartPosition, Vector3 EndPosition, float Vellocity = 30, int arc = 60)
-    //{
-    //    bullet._startPosition = StartPosition;
-    //    bullet._endPosition = EndPosition;
-    //    bullet._vellocity = Vellocity;
-    //    bullet._arc = 60;
-    //}
-
-
-
 }
