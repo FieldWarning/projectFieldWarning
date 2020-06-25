863dc4e37cd3be41e18239c0db00b9e20d33706c
ctigrisht <43137837+ctigrisht@users.noreply.github.com>
2020-06-25 07:12:24 +0200

    added tester project

---
+++ pfw-server/Tester/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
